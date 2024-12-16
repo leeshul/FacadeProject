8a7ba7d344f61dad65614c701bb2f16a9109097f
leeshul <windshul@gmail.com>
2024-12-16 22:44:03 +0900

    feat : 캐릭터 이동로직 개선 (hug the wall)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,10 +12,41 @@ public class Player : MonoBehaviour {
 
     Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-    float playerSize = .7f;
+    float moveDistance = moveSpeed * Time.deltaTime;
+    float playerRadius = .7f;
+    float playerHeight = 2f;
 
-    bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize); 
-    // TODO : 벽 왼쪽으로 지나갈 때 player 몸이 일부 겹치는 문제
+
+    bool canMove;
+    canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance); 
+    
+    if (!canMove){
+      // Cannot move towards moveDir
+
+      // Attempt only X movement
+      Vector3 moveDirX  = new Vector3(moveDir.x, 0, 0).normalized;
+      canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance); 
+
+      if (canMove){
+        moveDir = moveDirX;
+      } else {
+        // Cannot move only on the X
+        
+        // Attempt only Z movement
+        Vector3 moveDirZ  = new Vector3(0, 0, moveDir.z).normalized;
+        canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance); 
+
+        if (canMove){
+          moveDir = moveDirZ;
+        } else {
+          // Cannot move in any direnction
+          
+          // TODO : 대각선 모서리에서 부자연스럽게 진행 못하는 문제.
+          // TODO : 움직임 방향이 8방향 밖에 없는 문제.
+        }
+      }
+    }
+    
     if (canMove){
       transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
