edfbc870fd9489a4fbcf5983c5bbaa3b7bfcbfe2
leeshul <windshul@gmail.com>
2024-12-16 20:25:23 +0900

    feat : Add WASD move control
    - GameInput 오브젝트.
    
    Close #2

--- Assets/Scripts/Default.cs
+++ Assets/Scripts/Default.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
-public class GameInput : MonoBehaviour
+public class Default : MonoBehaviour
 {
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
