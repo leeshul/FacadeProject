f94c198b474acd632779a606e69a9ea6991689b3
leeshul <windshul@gmail.com>
2024-12-18 19:49:10 +0900

    feat :  카운터 상호작용 추가
    - 빈 카운터 상호작용시 카운터 위에 KitchenObject 생성.
    - KitchenObject 목록 : Tomato, CheeseBlock

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Interact()
     {
         Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
     }
 
     // Update is called once per frame
